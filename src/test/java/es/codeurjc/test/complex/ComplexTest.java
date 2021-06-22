3c97d276af586d39e8d37b3d2dd5edce21588221
Maes95 <michel.maes95@gmail.com>
2021-06-23 01:15:36 +0200

    Upload code

---
+++ src/test/java/es/codeurjc/test/complex/ComplexTest.java
@@ -0,0 +1,47 @@
+package es.codeurjc.test.complex;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ComplexTest {
+
+	private Complex zero;
+
+	@BeforeEach
+	public void setUp() {
+		zero = new Complex(0, 0);
+	}
+
+	@Test
+	public void givenZeroComplex_thenRealPartZeroAndImagPartZero() {
+		assertEquals(0, zero.getRealPart(), 0.001);
+		assertEquals(0, zero.getImaginaryPart(), 0.001);
+	}
+
+	@Test
+	public void givenZeroComplex_whenAddToComplex1_1_thenComplex1_1IsObtained() {
+
+		assertEquals(new Complex(1, 1), zero.add(new Complex(1, 1)));
+	}
+
+	@Test
+	public void givenComplex1_1_whenAddToZero_thenComplex1_1IsObtained() {
+
+		assertEquals(new Complex(1, 1), (new Complex(1, 1)).add(zero));
+	}
+	
+	@Test
+	public void givenComplex0_0_whenReciprocal_thenExceptionIsThrown() {
+
+		ArithmeticException actual = assertThrows(ArithmeticException.class, () -> {
+			
+			zero.reciprocal();
+			
+		});
+
+		assertEquals(actual.getMessage(), "division by zero");
+	}
+}
